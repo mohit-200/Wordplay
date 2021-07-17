3311f85126c99e7f116cc58dd72ce2e43d421645
mohit-200 <82448012+mohit-200@users.noreply.github.com>
2021-07-17 22:10:00 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+export let infinitive = [
+  "run",
+  "eat",
+  "sleep",
+  "swim",
+  "play",
+  "walk",
+  "sit",
+];
+
+export let pastSimple = [
+  "delhi",
+  "cup",
+  "football",
+  "apple",
+  "court",
+];
+
+export let pastParticiple = [
+ "he",
+  "she",
+  "they",
+  "himself",
+  "it",
+];
